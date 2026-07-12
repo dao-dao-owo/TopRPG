37c032b7a4c2530b76bd6c6339b4b629f1ed9daf
姚沛然 <1926791740@qq.com>
2026-07-12 10:19:39 +0800

    增加敌人血条UI显示和刷新逻辑

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -5,21 +5,16 @@
 #include "CoreMinimal.h"
 #include "AuraCharacterBase.h"
 #include "Interaction/EnemyInterface.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
+
+class UWidgetComponent;
 
 UCLASS()
 class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 
-
-
-	
-
-protected:
-
-	virtual void BeginPlay() override;
-	virtual void InitAbilityActorInfo() override;
 
 public:
 
@@ -35,7 +30,20 @@ public:
 	virtual int32 GetPlayerLevel() override;
 	/* CombatInterface函数  end */
 	
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangedSignature OnHealthChanged;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangedSignature OnMaxHealthChanged;
+	
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void InitAbilityActorInfo() override;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
 	int32 Level = 1;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UWidgetComponent> HealthBar;
 };
